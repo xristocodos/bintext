c5e944142d8a4c9f0fe15550296e12865e297cdc
xristocodos <xristophonos@gmail.com>
2017-06-20 13:26:26 -0400

    got it working, now to fix a couple cosmetics

--- binary.rb
+++ binary.rb
@@ -47,9 +47,17 @@ print_vars(phrase_in, charclump)
 # output_prespace = "0110111100100000011010000110000101101001" #DEBUG
 
 output_prespace = ""
-output_prespace += phrase_in[0].unpack('B8')[0]
+char_itr = 0
+
+while char_itr < phrase_in.length
+  output_prespace += phrase_in[char_itr].unpack('B8')[0]
+  char_itr += 1
+end
+
 output_postspace = output_prespace.scan(/.{1,#{numclump}}/).join(' ')
-binding.pry
-puts "raw: #{output_prespace}"
+
+clear_screen
+print_vars(phrase_in, charclump)
+puts "raw: #{output_prespace}\n"
 puts "fmt: #{output_postspace}"
 
